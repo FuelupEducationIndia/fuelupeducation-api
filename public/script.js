fbc7f10c77458a940152cad7252ae8514aeec3f3
143priyanka <priyankagupta.cse@gmail.com>
2020-11-09 15:25:10 +0530

    socket issue

--- public/script.js
+++ public/script.js
@@ -1,38 +1,38 @@
- const socket = io('/')
- const mypeer = new Peer(undefined, {
-     host: '/',
-     port: '3030'
- });
- const videoGride = document.getElementById('video-grid');
+const socket = io(server);
+const mypeer = new Peer(undefined, {
+    host: '/',
+    port: '3030'
+});
+const videoGride = document.getElementById('video-grid');
 
- const myVideo = document.createElement('video');
+const myVideo = document.createElement('video');
 
- myVideo.muted = true;
+myVideo.muted = true;
 
- mypeer.on('open', (id) => {
-     socket.emit('join-room', ROOM_ID, id);
- });
+mypeer.on('open', (id) => {
+    socket.emit('join-room', ROOM_ID, id);
+});
 
- socket.on('user-connected', (userId) => {
-     console.log(`User connected : ${userId}`);
-     console.log('ssssssssss');
- });
+socket.on('user-connected', (userId) => {
+    console.log(`User connected : ${userId}`);
+    console.log('ssssssssss');
+});
 
- // socket.on('user-disconnected', (userId) => {
- //     console.log(`User disconnected : ${userId}`);
- // });
+// socket.on('user-disconnected', (userId) => {
+//     console.log(`User disconnected : ${userId}`);
+// });
 
- navigator.mediaDevices.getUserMedia({
-     video: true,
-     audio: true
- }).then((strem) => {
-     addVidioStrem(myVideo, strem);
- }).catch((err) => {});
+//  navigator.mediaDevices.getUserMedia({
+//      video: true,
+//      audio: true
+//  }).then((strem) => {
+//      addVidioStrem(myVideo, strem);
+//  }).catch((err) => {});
 
- function addVidioStrem(video, strem) {
-     video.srcObject = strem;
-     video.addEventListener('loadedmetadata', () => {
-         //video.play();
-     });
-     //videoGride.append(video);
- }
+//  function addVidioStrem(video, strem) {
+//      video.srcObject = strem;
+//      video.addEventListener('loadedmetadata', () => {
+//          //video.play();
+//      });
+//      //videoGride.append(video);
+//  }
