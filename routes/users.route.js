7992c448cd6fa4aa3958c1d535509448e8bff8e1
143priyanka <priyankagupta.cse@gmail.com>
2021-02-20 21:48:55 +0530

    resolve lecture conflict

--- routes/users.route.js
+++ routes/users.route.js
@@ -2,11 +2,8 @@ const express = require("express");
 const router = express.Router();
 const userControllers = require("../controllers/userControllers");
 const roomController = require("../controllers/roomController");
-<<<<<<< HEAD
 const lectureController = require('../controllers/lectureController');
-=======
 const tickets = require("../controllers/ticketController");
->>>>>>> master
 
 // api for singup
 router.post("/signup", userControllers.signup);
@@ -25,7 +22,6 @@ router.get(
 //api to create room id
 router.get("/createRoom", roomController.createRoomId);
 
-<<<<<<< HEAD
 //api to upload call recording
 router.post('/uploadRecording', roomController.uploadCallRecording);
 
@@ -36,11 +32,9 @@ router.get('/getAllLectures', lectureController.getAllLectures);
 router.get('/getLecturesById', lectureController.getLecturesById);
 router.delete('/deleteLectureById', lectureController.deleteLectureById);
 
-=======
 // api for tickets
 router.post('/createTicket', tickets.createTicket);
 router.put('/updateTicketById/:id', tickets.updateTicketById);
 router.delete('/deleteTicketById/:id', tickets.deleteTicketById);
 router.get('/getTicketsByStatus/:status', tickets.getTicketsByStatus);
->>>>>>> master
 module.exports = router;
